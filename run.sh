fcbda05931513c92496bdaf1c235ef7d0cc8c82c
aluizio <aluizio@dcc.ufrj.br>
2016-03-07 10:31:31 -0300

    Added the script to run the app

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ET="eth0"
+if [ "$1" != "" ]; then
+	ET=$1
+fi
+
+IP=""
+IP="$(ifconfig $ET | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }')"
+if [ "$IP" != "" ]; then
+	python3 manage.py runserver $IP:8000
+fi
+
