cb339b99f70707bd59f7ab092bce76fd60a00dda
aluizio <aluizio@dcc.ufrj.br>
2016-03-22 17:50:57 -0300

    Added the UF geometries

--- bcim/urls.py
+++ bcim/urls.py
@@ -12,7 +12,7 @@ urlpatterns = format_suffix_patterns([
     url(r'^estados/(?P<id_objeto>[0-9]*)/$', views.UnidadeFederacaoDetail.as_view(), name='uf_detail_id_objeto'),
     url(r'^estados/(?P<sigla>[A-Za-z]{2})/$', views.UnidadeFederacaoDetail.as_view(), name='uf_detail_sigla'),
     url(r'^estados/(?P<sigla>[A-Za-z]{2})/(?P<spatial_function_1>[A-Za-z0-9]+)/$', views.UnidadeFederacaoDetail.as_view(), name='uf_detail_si'),
-    #url(r'^estados/(?P<sigla>[A-Za-z]{2})/(?P<spatial_function_1>[A-Za-z0-9]+)/(?P<param_1>\.\d+\.)/$', views.UnidadeFederacaoDetail.as_view(), name='uf_detail_sigla_nu'),
+    url(r'^estados/(?P<sigla>[A-Za-z]{2})/(?P<spatial_function_1>[A-Za-z0-9]+)/(?P<param_1>\.\d+\.)/$', views.UnidadeFederacaoDetail.as_view(), name='uf_detail_sigla_nu'),
     url(r'^estados/(?P<sigla>[A-Za-z]{2})/(?P<spatial_function_1>[A-Za-z0-9]+)/(?P<param_1>[\{a-zA-Z&:.,"\[\(+\-0-9\s\]\)\}]*)/$', views.UnidadeFederacaoDetail.as_view(), name='uf_detail_sigla_vl'),
     url(r'^estados/(?P<sigla>[A-Za-z]{2})/(?P<spatial_function_1>[A-Za-z]+)/(?P<param_1>.*)/(?P<spatial_function_2>[A-Za-z]+)/$', views.UnidadeFederacaoDetail.as_view(), name='uf_detail_sigla'),
     url(r'^estados/(?P<sigla>[A-Za-z]{2})/(?P<spatial_function_1>[A-Za-z0-9]+)/(?P<spatial_function_2>[A-Za-z0-9]+)/$', views.UnidadeFederacaoDetail.as_view(), name='uf_detail_si'),
@@ -20,9 +20,9 @@ urlpatterns = format_suffix_patterns([
 
 
     #Collection estados
-    # url(r'^estados/$', views.UnidadeFederacaoListFilteredByQueryParameters.as_view(), name='uf_list' ),
-    # url(r'^estados/(?P<siglas>\w+(\s*,\s*\w+)*)/$', views.UnidadeFederacaoFiltered.as_view(), name='uf_list_sigla_filtered'),
-    # url(r'^estados/(?P<spatial_function>[A-Za-z]+)/(?P<geom>.*)/$', views.UnidadeFederacaoFiltered.as_view(), name='aldeia_uf_spatial_filtered'),
+    url(r'^estados/$', views.UnidadeFederacaoListFilteredByQueryParameters.as_view(), name='uf_list' ),
+    url(r'^estados/(?P<siglas>\w+(\s*,\s*\w+)*)/$', views.UnidadeFederacaoFiltered.as_view(), name='uf_list_sigla_filtered'),
+    url(r'^estados/(?P<spatial_function>[A-Za-z]+)/(?P<geom>.*)/$', views.UnidadeFederacaoFiltered.as_view(), name='aldeia_uf_spatial_filtered'),
 
     url(r'^aldeias-indigenas/(?P<id_objeto>[0-9]+)/$', views.AldeiaIndigenaDetail.as_view(), name='uf_detail_aldeia'),
     url(r'^aldeias-indigenas/(?P<id_objeto>[0-9]+)/(?P<spatial_function_1>[A-Za-z]+)/$', views.AldeiaIndigenaDetail.as_view(), name='aldeia_detail_sf'),
--- bcim/views.py
+++ bcim/views.py
@@ -52,7 +52,7 @@ from bcim.serializers import UnidadeFederacaoSerializer,MunicipioSerializer, Out
 @api_view(('GET',))
 def api_root(request, format=None):
     return Response({
-        #'unidades federativas': reverse('bcim_v1:uf_list', request=request, format=format),
+        'unidades federativas': reverse('bcim_v1:uf_list', request=request, format=format),
         'municipios': reverse('bcim_v1:municipio_list', request=request, format=format),
         'outras unidades protegidas': reverse('bcim_v1:outras_unid_protegidas_list', request=request, format=format),
         'outros limites oficiais': reverse('bcim_v1:outros_limites_oficiais_list', request=request, format=format),
