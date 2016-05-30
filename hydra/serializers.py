56887bbb36391103cc5242bff916509a55346e00
aluizio <aluizio@dcc.ufrj.br>
2016-05-30 18:38:14 -0300

    added the part of the code to add spatial functions.

--- hydra/serializers.py
+++ hydra/serializers.py
@@ -1,15 +1,23 @@
 from hydra.hydra import *
 from hydra.models import SupportedProperty, SupportedOperation
+from context.models import Class
 
 class HydraSerializer(HydraClassSerializer):
 
-    def __init__(self, classobject, request, classname):
+    def __init__(self, classobject, request, classname, spatial=None):
         super(HydraSerializer, self).__init__()
         self.request = request
         self.classobject = classobject
         self.class_name = classname
         self.properties_objects = SupportedProperty.objects.filter(hydra_class=classobject.id)
         self.operations_objects = SupportedOperation.objects.filter(hydra_class=classobject.id)
+        self.spatial_operations = self.getSpatialOperations(spatial)
+
+    def getSpatialOperations(self, spatial=None):
+        if spatial == "geometry":
+            geometry = Class.objects.filter(name="geometry")
+            return SupportedOperation.objects.filter(hydra_class=geometry.id)
+        return None
 
     def createProperties(self, property_serializer):
         for property in self.properties_objects:
@@ -18,6 +26,9 @@ class HydraSerializer(HydraClassSerializer):
     def createMethods(self, method_serializer):
         for method in self.operations_objects:
             method_serializer.addCustomOperation(type=method.type, title=method.title, httpMethod=method.method, expects=self.getURLClass(method.expects), returns=self.getURLClass(method.returns))
+        if self.spatial_operations is not None:
+            for method in self.spatial_operations:
+                method_serializer.addCustomOperation(type=method.type, title=method.title, httpMethod=method.method, expects=self.getURLClass(method.expects), returns=self.getURLClass(method.returns))
 
     def getURLClass(self, obj):
         if obj is not None:
--- hydra/utilities.py
+++ hydra/utilities.py
@@ -2,7 +2,7 @@ from context.models import *
 from hydra.models import *
 from hydra.serializers import *
 
-def getHydraData(classname, request):
+def getHydraData(classname, request, spatial=None):
     classobject = Class.objects.get(name=classname)
-    serializerHydra = HydraSerializer(classobject, request, classname)
+    serializerHydra = HydraSerializer(classobject, request, classname, spatial)
     return serializerHydra.data
