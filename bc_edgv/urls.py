31430a7bdace0cd624ee2964ac7046611ca3aa76
aluizio <aluizio@dcc.ufrj.br>
2016-08-24 14:10:54 -0300

    Added the hydra like a separated vocab.

--- bc_edgv/urls.py
+++ bc_edgv/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     url(r'^idehco3/bcedgv/ibge/bcim/', include('bcim.urls', namespace='bcim_v1')),
     url(r'^idehco3/bcedgv/contexts/', include('context.urls', namespace='context')),
+    url(r'^idehco3/bcedgv/hydra', include('hydra.urls', namespace='hydra'))
 
 
 ]
