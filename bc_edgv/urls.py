cfcabce6c41da5d8833f9290d55e1993dd374efb
aluizio <aluizio@dcc.ufrj.br>
2016-08-24 14:32:19 -0300

    improved hydra vocab link

--- bc_edgv/urls.py
+++ bc_edgv/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from hydra.urls import getHydraVocabURLPatterns
 
 urlpatterns = [
 
     url(r'^idehco3/bcedgv/ibge/bcim/', include('bcim.urls', namespace='bcim_v1')),
     url(r'^idehco3/bcedgv/contexts/', include('context.urls', namespace='context')),
-    url(r'^idehco3/bcedgv/hydra', include('hydra.urls', namespace='hydra'))
+    getHydraVocabURLPatterns(r'^idehco3/bcedgv/hydra/'),
 
 
 ]
