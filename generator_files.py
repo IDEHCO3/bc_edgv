1c315d9fff762448e58fbfb4a215f99124d38a7e
RogerioBorba <rlrb71@gmail.com>
2017-08-29 19:56:37 -0300

    choices to generate files

--- generator_files.py
+++ generator_files.py
@@ -1,9 +1,14 @@
 import os
 import sys, inspect
-
+import ast
 def main(argv):
-    is_spatial = True
-    if (len(argv)) < 3:
+    has_to_generate_models  = False
+    has_to_generate_views  = True
+    has_to_generate_urls  = True
+    has_to_generate_serializers  = True
+    has_to_generate_contexters  = True
+    size_of_arguments = len(argv)
+    if size_of_arguments < 3:
         print('Usage: python generator_files.py django_project_name django_app_name')
         exit()
     else:
@@ -13,8 +18,17 @@ def main(argv):
 
     prj_name = argv[1]
     app_name = argv[2]
-    if len(argv) == 4:
-        is_spatial = False if argv[3].lower().strip() == 'nospatial' else True
+
+    if size_of_arguments > 3:
+        has_to_generate_models = ast.literal_eval(argv[3])
+    if size_of_arguments > 4:
+        has_to_generate_views = ast.literal_eval(argv[4])
+    if size_of_arguments > 5:
+        has_to_generate_urls = ast.literal_eval(argv[5])
+    if size_of_arguments > 6:
+        has_to_generate_serializers = ast.literal_eval(argv[6])
+    if size_of_arguments > 7:
+        has_to_generate_contexters = ast.literal_eval(argv[7])
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", prj_name + ".settings")
 
@@ -27,27 +41,32 @@ def main(argv):
     from modeler_generator import generate_file as gf_modeler
     from django.contrib.gis.db.models.fields import GeometryField
     from django.conf import settings
-    django.setup()
-
+    
     file_model_app = app_name + '/models.py'
+    if has_to_generate_models:
+        os.system("python manage.py inspectdb > "+file_model_app)
+    django.setup()
     gf_modeler(app_name, default_name=file_model_app)
 
-    file_url_prj = prj_name + '/urls.py'
-    gf_prj_urler(app_name, default_name=file_url_prj)
+    if has_to_generate_views:
+        file_view = app_name + '/views.py'
+        gf_viewer(app_name, default_name=file_view)
 
-    file_view = app_name + '/views.py'
-    gf_viewer(app_name, default_name=file_view)
+    if has_to_generate_urls:
+        file_url_prj = prj_name + '/urls.py'
+        gf_prj_urler(app_name, default_name=file_url_prj)
+        file_url_app = app_name + '/urls.py'
+        gf_urler(app_name, default_name=file_url_app)
 
-    file_url_app = app_name + '/urls.py'
-    gf_urler(app_name, default_name=file_url_app)
+    if has_to_generate_serializers:
+        file_serializer_app = app_name + '/serializers.py'
+        gf_serializer(app_name, default_name=file_serializer_app)
 
-    file_serializer_app = app_name + '/serializers.py'
-    gf_serializer(app_name, default_name=file_serializer_app)
+    if has_to_generate_contexters:
+        file_contexter_app = app_name + '/contexts.py'
+        gf_contexter(app_name, default_name=file_contexter_app)
 
-    file_contexter_app = app_name + '/contexts.py'
-    gf_contexter(app_name, default_name=file_contexter_app)
-
-    print('views.py, urls.py, serialzers.py and contexts.py have been generated')
+    print('All files have been generated')
 
 
 if __name__ == "__main__":
